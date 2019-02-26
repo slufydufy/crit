78edd187969435a6beeace84c471d549ad1f0199
slufydufy <malfenro@gmail.com>
2019-02-26 08:27:17 +0700

    rmh260219

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,15 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.lime,
       ),
       home: MyHomePage(),
@@ -32,6 +24,90 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Crit\''),
       ),
+      body: ListView(
+        children: <Widget>[
+          _showCarousel(context),
+          _gridView(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _showCarousel(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Container(
+            height: height * 0.25,
+            padding: EdgeInsets.all(8.0),
+            child: new Carousel(
+              boxFit: BoxFit.cover,
+              images: [
+                NetworkImage(
+                    'https://static.thefancydeal.com/uploads/edd/2017/08/buy-tshirt-design.jpg'),
+                NetworkImage(
+                    'https://image.dhgate.com/0x0/f2/albu/g6/M00/74/9F/rBVaR1oySymAMz2TAAM9Nf6_oQA427.jpg'),
+                NetworkImage(
+                    'https://static.thefancydeal.com/uploads/edd/2018/01/Fearless-buy-t-shirt-design.jpg'),
+                NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm0BBJPR09Bj8Ft_TDxbJqqSpb2qBrJEC9NDGq2wfrN-0aHnZo')
+              ],
+              animationCurve: Curves.fastOutSlowIn,
+              animationDuration: Duration(milliseconds: 1500),
+              dotSize: 6.0,
+              indicatorBgPadding: 15.0,
+            ),
+          );
+  }
+
+  Widget _gridView(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(4.0, 0.0, 8.0, 4.0),
+      child: GridView.count(
+        childAspectRatio: MediaQuery.of(context).size.width /
+              (MediaQuery.of(context).size.height / 1.7),
+        physics: ScrollPhysics(),
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        children: List.generate(8, (index) {
+          return _itemCard(context);
+        })
+      ),
+    );
+  }
+
+  Widget _itemCard(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Container(
+      
+      child: new Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmuKYRLIe_TOCWpcCIlrtZy9DBG_-gXYQrwaEJokBTOIwGZcNffA',
+                  height: height * 0.18,
+                  // width: 150,
+                  fit: BoxFit.cover,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+                  child: new Text('Item Available U powers', style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold
+                  ),),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+                  child: new Text('Item Available', style: TextStyle(
+                    // fontSize: 18.0,
+                    // fontWeight: FontWeight.bold
+                  ),),
+                )
+              ],
+            ),
+          ),
+          
     );
   }
 }
+
