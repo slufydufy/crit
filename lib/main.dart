f45b8e7e0fff614549cdc0b4b55cdf267c3c8de3
malfenro <malfenro@JKTOSXNB024.local>
2019-02-27 18:22:58 +0700

    ktr270219

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,14 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Crit\''),
+        title: Center(child: Text('Crit')),
       ),
       body: ListView(
         children: <Widget>[
           _showCarousel(context),
+          _textProduct(),
           _gridView(context)
-        ],
+          ],
       ),
     );
   }
@@ -36,78 +37,157 @@ class MyHomePage extends StatelessWidget {
   Widget _showCarousel(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     return Container(
-            height: height * 0.25,
-            padding: EdgeInsets.all(8.0),
-            child: new Carousel(
-              boxFit: BoxFit.cover,
-              images: [
-                NetworkImage(
-                    'https://static.thefancydeal.com/uploads/edd/2017/08/buy-tshirt-design.jpg'),
-                NetworkImage(
-                    'https://image.dhgate.com/0x0/f2/albu/g6/M00/74/9F/rBVaR1oySymAMz2TAAM9Nf6_oQA427.jpg'),
-                NetworkImage(
-                    'https://static.thefancydeal.com/uploads/edd/2018/01/Fearless-buy-t-shirt-design.jpg'),
-                NetworkImage(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm0BBJPR09Bj8Ft_TDxbJqqSpb2qBrJEC9NDGq2wfrN-0aHnZo')
-              ],
-              animationCurve: Curves.fastOutSlowIn,
-              animationDuration: Duration(milliseconds: 1500),
-              dotSize: 6.0,
-              indicatorBgPadding: 15.0,
-            ),
-          );
+      height: height * 0.4,
+      padding: EdgeInsets.all(8.0),
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          NetworkImage(
+              'https://static.thefancydeal.com/uploads/edd/2017/08/buy-tshirt-design.jpg'),
+          NetworkImage(
+              'https://image.dhgate.com/0x0/f2/albu/g6/M00/74/9F/rBVaR1oySymAMz2TAAM9Nf6_oQA427.jpg'),
+          NetworkImage(
+              'https://static.thefancydeal.com/uploads/edd/2018/01/Fearless-buy-t-shirt-design.jpg'),
+          NetworkImage(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm0BBJPR09Bj8Ft_TDxbJqqSpb2qBrJEC9NDGq2wfrN-0aHnZo')
+        ],
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1500),
+        dotSize: 6.0,
+        indicatorBgPadding: 15.0,
+      ),
+    );
+  }
+
+  Widget _textProduct() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+      child: Text('Buy and Donate Now', style: TextStyle(
+        fontSize: 18.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.grey
+      ),
+      ),
+    );
   }
 
   Widget _gridView(BuildContext context) {
     return Container(
       padding: EdgeInsets.fromLTRB(4.0, 0.0, 8.0, 4.0),
       child: GridView.count(
-        childAspectRatio: MediaQuery.of(context).size.width /
-              (MediaQuery.of(context).size.height / 1.7),
-        physics: ScrollPhysics(),
-        shrinkWrap: true,
-        crossAxisCount: 2,
-        children: List.generate(8, (index) {
-          return _itemCard(context);
-        })
-      ),
+          childAspectRatio: MediaQuery.of(context).size.width /
+              (MediaQuery.of(context).size.width / 0.7),
+          physics: ScrollPhysics(),
+          shrinkWrap: true,
+          crossAxisCount: 2,
+          children: List.generate(4, (index) {
+            return _itemCard(context);
+          })),
     );
   }
 
   Widget _itemCard(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    return Container(
-      
-      child: new Card(
+    return FlatButton(
+        padding: EdgeInsets.all(0.0),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => ItemDetail()));
+        },
+        child: new Card(
             clipBehavior: Clip.antiAlias,
+            color: Colors.white,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                new Image.network(
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmuKYRLIe_TOCWpcCIlrtZy9DBG_-gXYQrwaEJokBTOIwGZcNffA',
-                  height: height * 0.18,
-                  // width: 150,
-                  fit: BoxFit.cover,
+                Center(
+                  child: Image.network(
+                    'https://www.redwolf.in/image/cache/catalog/mens-t-shirts/breaking-bad-official-heisenberg-t-shirt-india-438x438.jpg',
+                    height: MediaQuery.of(context).size.width / 2,
+                    fit: BoxFit.fitHeight, 
+                  ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
-                  child: new Text('Item Available U powers', style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold
-                  ),),
+                  padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 0.0),
+                  child: new Text(
+                    'Our new design : Mr.Crit in full face',
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.grey),
+                    maxLines: 2,
+                  ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
-                  child: new Text('Item Available', style: TextStyle(
-                    // fontSize: 18.0,
-                    // fontWeight: FontWeight.bold
-                  ),),
+                  padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 0.0),
+                  child: new Text(
+                    'IDR 125K',
+                    style: TextStyle(color: Colors.grey),
+                  ),
                 )
               ],
-            ),
-          ),
-          
-    );
+            )));
   }
 }
 
+class ItemDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _showImage(context),
+          _showTitle(),
+          _showPrice(),
+        ],
+      ),
+    );
+  }
+  Widget _showImage(BuildContext context) {
+    return Container(
+              padding: EdgeInsets.all(0.0),
+              child: Image.network(
+                'https://www.redwolf.in/image/cache/catalog/mens-t-shirts/breaking-bad-official-heisenberg-t-shirt-india-438x438.jpg',
+                  fit: BoxFit.cover,
+                  height: MediaQuery.of(context).size.height / 2,),
+            );
+  }
+
+  Widget _showTitle() {
+    return Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text('Our new design : Mr.Crit in full face', style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey
+                  ),
+                  maxLines: 2,),
+                ),
+                Text('135K', style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.lime
+                ),
+                maxLines: 2,)
+              ],
+            ),
+          );
+  }
+
+  Widget _showPrice() {
+    return Padding(
+            padding: EdgeInsets.fromLTRB(0.0, 16.0, 8.0, 0.0),
+            child: Text('Our new design : Mr.Crit in full face Our new design : Mr.Crit in full face', style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey
+            ),
+            maxLines: 2,),
+          );
+  }
+
+}
