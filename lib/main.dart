c73bdf87f2cd2667df7e8b797ca2a3bde1d7d0c5
malfenro <malfenro@JKTOSXNB024.local>
2019-02-28 20:24:51 +0700

    ktr280219

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+    .then((_) {
+      runApp(new MyApp());
+    });
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.lime,
       ),
       home: MyHomePage(),
     );
@@ -37,7 +43,7 @@ class MyHomePage extends StatelessWidget {
   Widget _showCarousel(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     return Container(
-      height: height * 0.4,
+      height: height * 0.3,
       padding: EdgeInsets.all(8.0),
       child: new Carousel(
         boxFit: BoxFit.cover,
@@ -65,7 +71,6 @@ class MyHomePage extends StatelessWidget {
       child: Text('Buy and Donate Now', style: TextStyle(
         fontSize: 18.0,
         fontWeight: FontWeight.bold,
-        // color: Colors.grey
       ),
       ),
     );
@@ -108,11 +113,14 @@ class MyHomePage extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 0.0),
+                  padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
                   child: new Text(
-                    'Our new design : Mr.Crit in full face',
+                    'Mr.Crit in full face',
                     style:
-                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.grey),
+                        TextStyle(fontSize: 16.0, 
+                        fontWeight: FontWeight.bold, 
+                        color: Colors.grey,
+                        ),
                     maxLines: 2,
                   ),
                 ),
@@ -135,37 +143,69 @@ class ItemDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text('Detail'),
       ),
-      body: ListView(
+      body: 
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          _showImage(context),
-          _showTitle(),
-          _showPrice(),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _showImage(context),
+                _showPrice(),
+                _showTitleDesc(),
+                Divider(),
+                _showMaterial(),
+                _showSize(),
+                Divider(),
+              ],
+            ),
+          ),
+          _showButton(context)
         ],
       ),
     );
   }
   Widget _showImage(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return Container(
-              padding: EdgeInsets.all(0.0),
-              child: Image.network(
-                'https://www.redwolf.in/image/cache/catalog/mens-t-shirts/breaking-bad-official-heisenberg-t-shirt-india-438x438.jpg',
-                  fit: BoxFit.cover,
-                  height: MediaQuery.of(context).size.height / 2,),
-            );
+      height: height * 0.5,
+      padding: EdgeInsets.all(8.0),
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          NetworkImage(
+              'https://static.thefancydeal.com/uploads/edd/2017/08/buy-tshirt-design.jpg'),
+          NetworkImage(
+              'https://image.dhgate.com/0x0/f2/albu/g6/M00/74/9F/rBVaR1oySymAMz2TAAM9Nf6_oQA427.jpg'),
+          NetworkImage(
+              'https://static.thefancydeal.com/uploads/edd/2018/01/Fearless-buy-t-shirt-design.jpg'),
+        ],
+        // animationCurve: Curves.fastOutSlowIn,
+        // animationDuration: Duration(milliseconds: 0),
+        autoplay: false,
+        dotSize: 6.0,
+        indicatorBgPadding: 6.0,
+        dotBgColor: Colors.transparent,
+        dotColor: Colors.grey,
+        
+      ),
+    );
   }
 
-  Widget _showTitle() {
+  Widget _showPrice() {
     return Padding(
             padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: new Container(color: Colors.lime, width: 16.0, height: 2.0,),
+                  child: new Container(child: Text('Mr.Crit in full face', style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.grey,
+                        ),
+                        maxLines: 2,),),
                 ),
-                
                 Text('IDR 135K', style: TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
                   color: Colors.lime
                 ),
                 maxLines: 2,)
@@ -174,26 +214,167 @@ class ItemDetail extends StatelessWidget {
           );
   }
 
-  Widget _showPrice() {
+  Widget _showTitleDesc() {
     return Padding(
-            padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+            padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
             child: Column(
               children: <Widget>[
-                Center(
-                  child: Text('Mr.Crit in full face', style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey
-                        ),
-                        maxLines: 2,),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Our new design : Mr.Crit in full face is inspired by the some one we call HERO in our new community. A nice old man who strugle from day to night that inspired us that life is not about complaint. '),
-                )
+                Text('Our new design : Mr.Crit in full face is inspired by the some one we call HERO in our new community. A nice old man who strugle from day to night that inspired us that life is not about complaint.',
+                style: TextStyle(
+                  letterSpacing: 0.5
+                  ),)
               ],
             ),
           );
+  }
+
+  Widget _showMaterial() {
+    return Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              // color: Colors.white70,
+              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 16.0),
+              child: Text(
+                'Material', style: TextStyle(
+                  color: Colors.grey
+                ),
+              ),
+            ),
+          ),
+          Container(
+            // color: Colors.white70,
+            padding: const EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 16.0),
+            child: Text(
+                'Cotton Combbed 30s', style: TextStyle(
+                  color: Colors.lime
+                ),
+              ),
+          ),
+        ],
+      );
+  }
+
+  Widget _showSize() {
+    return Row(
+        children: <Widget>[
+          Expanded(
+              child: Container(
+              // color: Colors.white70,
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 16.0),
+              child: Text(
+                'Available Size', style: TextStyle(
+                  color: Colors.grey
+                ),
+              ),
+            ),
+          ),
+          Container(
+            // color: Colors.white70,
+            padding: const EdgeInsets.fromLTRB(0.0, 8.0, 16.0, 16.0),
+            child: Text(
+                'M / L / XL', style: TextStyle(
+                  color: Colors.lime
+                ),
+              ),
+          ),
+        ],
+      );
+  }
+
+  Widget _showButton(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+      child: RaisedButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => CheckOut()));
+        },
+        color: Colors.lime,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0)
+        ),
+        child: Text('Buy Now', style: TextStyle(
+          color: Colors.white
+        ),),
+      ),
+    );
+  }
+
+}
+
+class CheckOut extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CheckOut'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _showBuyerInfoText(),
+          Divider(),
+          _showName(),
+          _showMobile(),
+          _showAddress(),
+          _showItemInfoText(),
+          Divider(),
+        ],
+      ),
+    );
+  }
+
+  Widget _showBuyerInfoText() {
+    return Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+          child: Text('Info Pembeli', style: TextStyle(
+            fontSize: 18.0
+          ),),
+        );
+  }
+
+  Widget _showName() {
+    return Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0.0),
+            child: TextFormField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                labelText: 'Nama',
+                border: OutlineInputBorder())
+            ),
+          );
+  }
+
+  Widget _showMobile() {
+    return Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0.0),
+            child: TextFormField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                labelText: 'Nomor HP',
+                border: OutlineInputBorder()),
+            ),
+          );
+  }
+
+  Widget _showAddress() {
+    return Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 24.0),
+            child: TextFormField(
+              maxLines: 5,
+              decoration: InputDecoration(
+                labelText: 'Alamat Pengiriman',
+                border: OutlineInputBorder()),
+            ),
+          );
+  }
+
+  Widget _showItemInfoText() {
+    return Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+          child: Text('Info Barang', style: TextStyle(
+            fontSize: 18.0
+          ),),
+        );
   }
 
 }
